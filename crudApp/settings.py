aa6148084d5757c6c7bb12fe571f1e49ad5c1d04
abecus <insaaone@gmail.com>
2020-07-19 00:47:06 +0530

    django_heroku-settings

--- crudApp/settings.py
+++ crudApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = '-h^xqi!ifzu9u*s)ie$rqy_$^7@l3qezp6o+n6f&v@s$9rp_oi'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "192.168.0.101",
+    # "192.168.0.101",
     'djangocrudapp.herokuapp.com',
 ]
 
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
